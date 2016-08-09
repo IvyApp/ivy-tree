9b5bdd170e9da1a674829ac831d0506c6507f604
Dray Lacy <dray@envylabs.com>
2016-08-09 16:30:25 -0400

    Remove shared test template
    This commit splits the giant monolithic test template into smaller, more
    focused pieces.

--- tests/integration/components/ivy-tree-test.js
+++ tests/integration/components/ivy-tree-test.js
@@ -3,119 +3,20 @@ import hbs from 'htmlbars-inline-precompile';
 
 import Ember from 'ember';
 
-const testTemplate = hbs`
-  {{#ivy-tree as |tree|}}
-    {{#tree.treeitem as |treeitem|}}
-      Animals
-      {{#treeitem.group as |group|}}
-        {{#group.treeitem as |treeitem|}}
-          Birds
-        {{/group.treeitem}}
-        {{#group.treeitem as |treeitem|}}
-          Cats
-          {{#treeitem.group as |group|}}
-            {{#group.treeitem}}
-              Siamese
-            {{/group.treeitem}}
-            {{#group.treeitem}}
-              Tabby
-            {{/group.treeitem}}
-          {{/treeitem.group}}
-        {{/group.treeitem}}
-        {{#group.treeitem as |treeitem|}}
-          Dogs
-          {{#treeitem.group as |group|}}
-            {{#group.treeitem as |treeitem|}}
-              Small Breeds
-              {{#treeitem.group as |group|}}
-                {{#group.treeitem}}
-                  Chihuahua
-                {{/group.treeitem}}
-                {{#group.treeitem}}
-                  Italian Greyhound
-                {{/group.treeitem}}
-                {{#group.treeitem}}
-                  Japanese Chin
-                {{/group.treeitem}}
-              {{/treeitem.group}}
-            {{/group.treeitem}}
-            {{#group.treeitem as |treeitem|}}
-              Medium Breeds
-              {{#treeitem.group as |group|}}
-                {{#group.treeitem}}
-                  Beagle
-                {{/group.treeitem}}
-                {{#group.treeitem}}
-                  Cocker Spaniel
-                {{/group.treeitem}}
-                {{#group.treeitem}}
-                  Pit Bull
-                {{/group.treeitem}}
-              {{/treeitem.group}}
-            {{/group.treeitem}}
-            {{#group.treeitem as |treeitem|}}
-              Large Breeds
-              {{#treeitem.group as |group|}}
-                {{#group.treeitem}}
-                  Afghan
-                {{/group.treeitem}}
-                {{#group.treeitem}}
-                  Great Dane
-                {{/group.treeitem}}
-                {{#group.treeitem}}
-                  Mastiff
-                {{/group.treeitem}}
-              {{/treeitem.group}}
-            {{/group.treeitem}}
-          {{/treeitem.group}}
-        {{/group.treeitem}}
-      {{/treeitem.group}}
-    {{/tree.treeitem}}
-    {{#tree.treeitem as |treeitem|}}
-      Minerals
-      {{#treeitem.group as |group|}}
-        {{#group.treeitem}}
-          Zinc
-        {{/group.treeitem}}
-        {{#group.treeitem as |treeitem|}}
-          Gold
-          {{#treeitem.group as |group|}}
-            {{#group.treeitem}}
-              Yellow Gold
-            {{/group.treeitem}}
-            {{#group.treeitem}}
-              White Gold
-            {{/group.treeitem}}
-          {{/treeitem.group}}
-        {{/group.treeitem}}
-        {{#group.treeitem}}
-          Silver
-        {{/group.treeitem}}
-      {{/treeitem.group}}
-    {{/tree.treeitem}}
-    {{#tree.treeitem as |treeitem|}}
-      Vegetables
-      {{#treeitem.group as |group|}}
-        {{#group.treeitem}}
-          Carrot
-        {{/group.treeitem}}
-        {{#group.treeitem}}
-          Tomato
-        {{/group.treeitem}}
-        {{#group.treeitem}}
-          Lettuce
-        {{/group.treeitem}}
-      {{/treeitem.group}}
-    {{/tree.treeitem}}
-  {{/ivy-tree}}
-`;
-
 moduleForComponent('ivy-tree', 'Integration | Component | ivy-tree', {
   integration: true
 });
 
 test('it applies an "aria-hidden" attribute to the children of a collapsed treeitem', function(assert) {
-  this.render(testTemplate);
+  this.render(hbs`
+    {{#ivy-tree as |tree|}}
+      {{#tree.treeitem as |treeitem|}}
+        {{#treeitem.group as |group|}}
+          {{group.treeitem}}
+        {{/treeitem.group}}
+      {{/tree.treeitem}}
+    {{/ivy-tree}}
+  `);
 
   assert.equal(this.$('[role="treeitem"]:eq(0)').attr('aria-expanded'), 'false');
   assert.equal(this.$('[role="treeitem"]:eq(0) [role="treeitem"]:eq(0)').attr('aria-hidden'), 'true');
@@ -125,39 +26,56 @@ test('it applies an "aria-hidden" attribute to the children of a collapsed treei
 });
 
 test('it applies an "aria-level" attribute to treeitems', function(assert) {
-  this.render(testTemplate);
+  this.render(hbs`
+    {{#ivy-tree as |tree|}}
+      {{#tree.treeitem as |treeitem|}}
+        {{#treeitem.group as |group|}}
+          {{group.treeitem}}
+        {{/treeitem.group}}
+      {{/tree.treeitem}}
+    {{/ivy-tree}}
+  `);
 
-  assert.equal(this.$('[role="treeitem"][aria-level="1"]').length, 3);
-  assert.equal(this.$('[role="treeitem"][aria-level="2"]').length, 9);
-  assert.equal(this.$('[role="treeitem"][aria-level="3"]').length, 7);
-  assert.equal(this.$('[role="treeitem"][aria-level="4"]').length, 9);
-  assert.equal(this.$('[role="treeitem"][aria-level="5"]').length, 0);
+  assert.equal(this.$('[role="treeitem"]:eq(0)').attr('aria-level'), '1');
+  assert.equal(this.$('[role="treeitem"]:eq(1)').attr('aria-level'), '2');
 });
 
 test('it applies an "aria-selected" attribute to the active treeitem', function(assert) {
-  this.render(testTemplate);
+  this.render(hbs`
+    {{#ivy-tree as |tree|}}
+      {{tree.treeitem}}
+    {{/ivy-tree}}
+  `);
 
-  assert.equal(this.$('[role="treeitem"][aria-level="1"]:eq(0)').attr('aria-selected'), 'false');
+  assert.equal(this.$('[role="treeitem"]:eq(0)').attr('aria-selected'), 'false');
 
-  this.$('[role="treeitem"][aria-level="1"]:eq(0)').click();
-  assert.equal(this.$('[role="treeitem"][aria-level="1"]:eq(0)').attr('aria-selected'), 'true');
+  this.$('[role="treeitem"]:eq(0)').click();
+  assert.equal(this.$('[role="treeitem"]:eq(0)').attr('aria-selected'), 'true');
 });
 
 test('it activates a treeitem on click', function(assert) {
-  this.render(testTemplate);
+  this.render(hbs`
+    {{#ivy-tree as |tree|}}
+      {{tree.treeitem}}
+    {{/ivy-tree}}
+  `);
 
   assert.equal(this.$('[role="tree"]').attr('aria-activedescendant'), undefined);
 
-  this.$('[role="treeitem"][aria-level="1"]:eq(0)').click();
-  assert.equal(this.$('[role="tree"]').attr('aria-activedescendant'), this.$('[role="treeitem"][aria-level="1"]:eq(0)').attr('id'));
+  this.$('[role="treeitem"]:eq(0)').click();
+  assert.equal(this.$('[role="tree"]').attr('aria-activedescendant'), this.$('[role="treeitem"]:eq(0)').attr('id'));
 });
 
 test('it applies an "active" class to the active treeitem', function(assert) {
-  this.render(testTemplate);
+  this.render(hbs`
+    {{#ivy-tree as |tree|}}
+      {{tree.treeitem}}
+    {{/ivy-tree}}
+  `);
 
-  this.$('[role="treeitem"][aria-level="1"]:eq(0)').click();
+  this.$('[role="treeitem"]:eq(0)').click();
 
-  assert.ok(this.$('[role="treeitem"][aria-level="1"]:eq(0)').hasClass('active'));
+  assert.ok(this.$('[role="treeitem"]:eq(0)').hasClass('active'));
 });
 
 test('it applies a custom activeClass class to the active treeitem', function(assert) {
@@ -173,7 +91,15 @@ test('it applies a custom activeClass class to the active treeitem', function(as
 });
 
 test('it toggles expansion of a treeitem on double-click', function(assert) {
-  this.render(testTemplate);
+  this.render(hbs`
+    {{#ivy-tree as |tree|}}
+      {{#tree.treeitem as |treeitem|}}
+        {{#treeitem.group as |group|}}
+          {{group.treeitem}}
+        {{/treeitem.group}}
+      {{/tree.treeitem}}
+    {{/ivy-tree}}
+  `);
 
   assert.equal(this.$('[role="treeitem"][aria-level="1"]:eq(0)').attr('aria-expanded'), 'false');
 
@@ -182,7 +108,15 @@ test('it toggles expansion of a treeitem on double-click', function(assert) {
 });
 
 test('it toggles expansion of the focused treeitem when enter key is pressed', function(assert) {
-  this.render(testTemplate);
+  this.render(hbs`
+    {{#ivy-tree as |tree|}}
+      {{#tree.treeitem as |treeitem|}}
+        {{#treeitem.group as |group|}}
+          {{group.treeitem}}
+        {{/treeitem.group}}
+      {{/tree.treeitem}}
+    {{/ivy-tree}}
+  `);
 
   this.$('[role="treeitem"][aria-level="1"]:eq(0)').click();
 
@@ -193,18 +127,31 @@ test('it toggles expansion of the focused treeitem when enter key is pressed', f
 });
 
 test('it selects the previous sibling treeitem when up arrow key is pressed', function(assert) {
-  this.render(testTemplate);
+  this.render(hbs`
+    {{#ivy-tree as |tree|}}
+      {{tree.treeitem}}
+      {{tree.treeitem}}
+    {{/ivy-tree}}
+  `);
 
-  this.$('[role="treeitem"][aria-level="1"]:eq(1)').click();
+  this.$('[role="treeitem"]:eq(1)').click();
 
   this.$('[role="tree"]').trigger(Ember.$.Event('keydown', { keyCode: 38 }));
-  assert.equal(this.$('[role="tree"]').attr('aria-activedescendant'), this.$('[role="treeitem"][aria-level="1"]:eq(0)').attr('id'));
+  assert.equal(this.$('[role="tree"]').attr('aria-activedescendant'), this.$('[role="treeitem"]:eq(0)').attr('id'));
 });
 
 test('it selects the parent treeitem when up arrow key is pressed', function(assert) {
-  this.render(testTemplate);
+  this.render(hbs`
+    {{#ivy-tree as |tree|}}
+      {{#tree.treeitem as |treeitem|}}
+        {{#treeitem.group as |group|}}
+          {{group.treeitem}}
+        {{/treeitem.group}}
+      {{/tree.treeitem}}
+    {{/ivy-tree}}
+  `);
 
-  this.$('[role="treeitem"][aria-level="1"]:eq(1)').dblclick();
+  this.$('[role="treeitem"][aria-level="1"]:eq(0)').dblclick();
   this.$('[role="treeitem"][aria-level="1"] [role="treeitem"][aria-level="2"]:eq(0)').click();
 
   this.$('[role="tree"]').trigger(Ember.$.Event('keydown', { keyCode: 38 }));
@@ -212,26 +159,48 @@ test('it selects the parent treeitem when up arrow key is pressed', function(ass
 });
 
 test('it selects the next sibling treeitem when down arrow key is pressed', function(assert) {
-  this.render(testTemplate);
+  this.render(hbs`
+    {{#ivy-tree as |tree|}}
+      {{tree.treeitem}}
+      {{tree.treeitem}}
+    {{/ivy-tree}}
+  `);
 
-  this.$('[role="treeitem"][aria-level="1"]:eq(0)').click();
+  this.$('[role="treeitem"]:eq(0)').click();
 
   this.$('[role="tree"]').trigger(Ember.$.Event('keydown', { keyCode: 40 }));
-  assert.equal(this.$('[role="tree"]').attr('aria-activedescendant'), this.$('[role="treeitem"][aria-level="1"]:eq(1)').attr('id'));
+  assert.equal(this.$('[role="tree"]').attr('aria-activedescendant'), this.$('[role="treeitem"]:eq(1)').attr('id'));
 });
 
 test('it selects the next sibling of the parent treeitem when down arrow key is pressed', function(assert) {
-  this.render(testTemplate);
+  this.render(hbs`
+    {{#ivy-tree as |tree|}}
+      {{#tree.treeitem as |treeitem|}}
+        {{#treeitem.group as |group|}}
+          {{group.treeitem}}
+        {{/treeitem.group}}
+      {{/tree.treeitem}}
+      {{tree.treeitem}}
+    {{/ivy-tree}}
+  `);
 
   this.$('[role="treeitem"][aria-level="1"]:eq(0)').dblclick();
-  this.$('[role="treeitem"][aria-level="1"]:eq(0) [role="treeitem"][aria-level="2"]:last-child').click();
+  this.$('[role="treeitem"][aria-level="1"]:eq(0) [role="treeitem"][aria-level="2"]:eq(0)').click();
 
   this.$('[role="tree"]').trigger(Ember.$.Event('keydown', { keyCode: 40 }));
   assert.equal(this.$('[role="tree"]').attr('aria-activedescendant'), this.$('[role="treeitem"][aria-level="1"]:eq(1)').attr('id'));
 });
 
 test('it collapses the (expanded) currently-focused node when left arrow key is pressed', function(assert) {
-  this.render(testTemplate);
+  this.render(hbs`
+    {{#ivy-tree as |tree|}}
+      {{#tree.treeitem as |treeitem|}}
+        {{#treeitem.group as |group|}}
+          {{group.treeitem}}
+        {{/treeitem.group}}
+      {{/tree.treeitem}}
+    {{/ivy-tree}}
+  `);
 
   this.$('[role="treeitem"][aria-level="1"]:eq(0)').click();
   this.$('[role="treeitem"][aria-level="1"]:eq(0)').dblclick();
@@ -245,7 +214,15 @@ test('it collapses the (expanded) currently-focused node when left arrow key is 
 });
 
 test('it selects the parent of the (collapsed) currently-focused node when left arrow key is pressed', function(assert) {
-  this.render(testTemplate);
+  this.render(hbs`
+    {{#ivy-tree as |tree|}}
+      {{#tree.treeitem as |treeitem|}}
+        {{#treeitem.group as |group|}}
+          {{group.treeitem}}
+        {{/treeitem.group}}
+      {{/tree.treeitem}}
+    {{/ivy-tree}}
+  `);
 
   this.$('[role="treeitem"][aria-level="1"]:eq(0)').dblclick();
   this.$('[role="treeitem"][aria-level="1"] [role="treeitem"][aria-level="2"]:eq(0)').click();
@@ -257,7 +234,15 @@ test('it selects the parent of the (collapsed) currently-focused node when left 
 });
 
 test('it expands a collapsed treeitem and selects its first child when right arrow key is pressed', function(assert) {
-  this.render(testTemplate);
+  this.render(hbs`
+    {{#ivy-tree as |tree|}}
+      {{#tree.treeitem as |treeitem|}}
+        {{#treeitem.group as |group|}}
+          {{group.treeitem}}
+        {{/treeitem.group}}
+      {{/tree.treeitem}}
+    {{/ivy-tree}}
+  `);
 
   this.$('[role="treeitem"][aria-level="1"]:eq(0)').click();
 
@@ -269,7 +254,15 @@ test('it expands a collapsed treeitem and selects its first child when right arr
 });
 
 test('it expands all treeitems when asterisk (shift+8) key is pressed', function(assert) {
-  this.render(testTemplate);
+  this.render(hbs`
+    {{#ivy-tree as |tree|}}
+      {{#tree.treeitem as |treeitem|}}
+        {{#treeitem.group as |group|}}
+          {{group.treeitem}}
+        {{/treeitem.group}}
+      {{/tree.treeitem}}
+    {{/ivy-tree}}
+  `);
 
   assert.notEqual(this.$('[role="treeitem"][aria-expanded="false"]').length, 0);
 
@@ -278,7 +271,12 @@ test('it expands all treeitems when asterisk (shift+8) key is pressed', function
 });
 
 test('it selects the first, visible treeitem when home key is pressed', function(assert) {
-  this.render(testTemplate);
+  this.render(hbs`
+    {{#ivy-tree as |tree|}}
+      {{tree.treeitem}}
+      {{tree.treeitem}}
+    {{/ivy-tree}}
+  `);
 
   this.$('[role="treeitem"][aria-level="1"]:eq(1)').click();
 
@@ -290,7 +288,17 @@ test('it selects the first, visible treeitem when home key is pressed', function
 });
 
 test('it selects the last, visible treeitem when end key is pressed', function(assert) {
-  this.render(testTemplate);
+  this.render(hbs`
+    {{#ivy-tree as |tree|}}
+      {{tree.treeitem}}
+      {{tree.treeitem}}
+      {{#tree.treeitem as |treeitem|}}
+        {{#treeitem.group as |group|}}
+          {{group.treeitem}}
+        {{/treeitem.group}}
+      {{/tree.treeitem}}
+    {{/ivy-tree}}
+  `);
 
   this.$('[role="treeitem"][aria-level="1"]:eq(1)').click();
 
@@ -304,7 +312,7 @@ test('it selects the last, visible treeitem when end key is pressed', function(a
   this.$('[role="treeitem"][aria-level="1"]:eq(2)').dblclick();
   this.$('[role="tree"]').trigger(Ember.$.Event('keydown', { keyCode: 35 }));
 
-  assert.equal(this.$('[role="treeitem"][aria-level="2"]:eq(8)').attr('aria-selected'), 'true');
+  assert.equal(this.$('[role="treeitem"][aria-level="2"]:eq(0)').attr('aria-selected'), 'true');
 });
 
 test('it sends an "onSelect" action when a treeitem is selected', function(assert) {
