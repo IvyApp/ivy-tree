5a1cf79f295e0718dc6b7d849b4f5bb0e8af03f1
Dray Lacy <dray@envylabs.com>
2016-08-09 16:15:41 -0400

    Add a minimal test case for `activeClass`

--- tests/integration/components/ivy-tree-test.js
+++ tests/integration/components/ivy-tree-test.js
@@ -3,120 +3,120 @@ import hbs from 'htmlbars-inline-precompile';
 
 import Ember from 'ember';
 
-moduleForComponent('ivy-tree', 'Integration | Component | ivy-tree', {
-  integration: true,
+const testTemplate = hbs`
+  {{#ivy-tree as |tree|}}
+    {{#tree.treeitem as |treeitem|}}
+      Animals
+      {{#treeitem.group as |group|}}
+        {{#group.treeitem as |treeitem|}}
+          Birds
+        {{/group.treeitem}}
+        {{#group.treeitem as |treeitem|}}
+          Cats
+          {{#treeitem.group as |group|}}
+            {{#group.treeitem}}
+              Siamese
+            {{/group.treeitem}}
+            {{#group.treeitem}}
+              Tabby
+            {{/group.treeitem}}
+          {{/treeitem.group}}
+        {{/group.treeitem}}
+        {{#group.treeitem as |treeitem|}}
+          Dogs
+          {{#treeitem.group as |group|}}
+            {{#group.treeitem as |treeitem|}}
+              Small Breeds
+              {{#treeitem.group as |group|}}
+                {{#group.treeitem}}
+                  Chihuahua
+                {{/group.treeitem}}
+                {{#group.treeitem}}
+                  Italian Greyhound
+                {{/group.treeitem}}
+                {{#group.treeitem}}
+                  Japanese Chin
+                {{/group.treeitem}}
+              {{/treeitem.group}}
+            {{/group.treeitem}}
+            {{#group.treeitem as |treeitem|}}
+              Medium Breeds
+              {{#treeitem.group as |group|}}
+                {{#group.treeitem}}
+                  Beagle
+                {{/group.treeitem}}
+                {{#group.treeitem}}
+                  Cocker Spaniel
+                {{/group.treeitem}}
+                {{#group.treeitem}}
+                  Pit Bull
+                {{/group.treeitem}}
+              {{/treeitem.group}}
+            {{/group.treeitem}}
+            {{#group.treeitem as |treeitem|}}
+              Large Breeds
+              {{#treeitem.group as |group|}}
+                {{#group.treeitem}}
+                  Afghan
+                {{/group.treeitem}}
+                {{#group.treeitem}}
+                  Great Dane
+                {{/group.treeitem}}
+                {{#group.treeitem}}
+                  Mastiff
+                {{/group.treeitem}}
+              {{/treeitem.group}}
+            {{/group.treeitem}}
+          {{/treeitem.group}}
+        {{/group.treeitem}}
+      {{/treeitem.group}}
+    {{/tree.treeitem}}
+    {{#tree.treeitem as |treeitem|}}
+      Minerals
+      {{#treeitem.group as |group|}}
+        {{#group.treeitem}}
+          Zinc
+        {{/group.treeitem}}
+        {{#group.treeitem as |treeitem|}}
+          Gold
+          {{#treeitem.group as |group|}}
+            {{#group.treeitem}}
+              Yellow Gold
+            {{/group.treeitem}}
+            {{#group.treeitem}}
+              White Gold
+            {{/group.treeitem}}
+          {{/treeitem.group}}
+        {{/group.treeitem}}
+        {{#group.treeitem}}
+          Silver
+        {{/group.treeitem}}
+      {{/treeitem.group}}
+    {{/tree.treeitem}}
+    {{#tree.treeitem as |treeitem|}}
+      Vegetables
+      {{#treeitem.group as |group|}}
+        {{#group.treeitem}}
+          Carrot
+        {{/group.treeitem}}
+        {{#group.treeitem}}
+          Tomato
+        {{/group.treeitem}}
+        {{#group.treeitem}}
+          Lettuce
+        {{/group.treeitem}}
+      {{/treeitem.group}}
+    {{/tree.treeitem}}
+  {{/ivy-tree}}
+`;
 
-  beforeEach() {
-    this.render(hbs`
-      {{#ivy-tree as |tree|}}
-        {{#tree.treeitem as |treeitem|}}
-          Animals
-          {{#treeitem.group as |group|}}
-            {{#group.treeitem as |treeitem|}}
-              Birds
-            {{/group.treeitem}}
-            {{#group.treeitem as |treeitem|}}
-              Cats
-              {{#treeitem.group as |group|}}
-                {{#group.treeitem}}
-                  Siamese
-                {{/group.treeitem}}
-                {{#group.treeitem}}
-                  Tabby
-                {{/group.treeitem}}
-              {{/treeitem.group}}
-            {{/group.treeitem}}
-            {{#group.treeitem as |treeitem|}}
-              Dogs
-              {{#treeitem.group as |group|}}
-                {{#group.treeitem as |treeitem|}}
-                  Small Breeds
-                  {{#treeitem.group as |group|}}
-                    {{#group.treeitem}}
-                      Chihuahua
-                    {{/group.treeitem}}
-                    {{#group.treeitem}}
-                      Italian Greyhound
-                    {{/group.treeitem}}
-                    {{#group.treeitem}}
-                      Japanese Chin
-                    {{/group.treeitem}}
-                  {{/treeitem.group}}
-                {{/group.treeitem}}
-                {{#group.treeitem as |treeitem|}}
-                  Medium Breeds
-                  {{#treeitem.group as |group|}}
-                    {{#group.treeitem}}
-                      Beagle
-                    {{/group.treeitem}}
-                    {{#group.treeitem}}
-                      Cocker Spaniel
-                    {{/group.treeitem}}
-                    {{#group.treeitem}}
-                      Pit Bull
-                    {{/group.treeitem}}
-                  {{/treeitem.group}}
-                {{/group.treeitem}}
-                {{#group.treeitem as |treeitem|}}
-                  Large Breeds
-                  {{#treeitem.group as |group|}}
-                    {{#group.treeitem}}
-                      Afghan
-                    {{/group.treeitem}}
-                    {{#group.treeitem}}
-                      Great Dane
-                    {{/group.treeitem}}
-                    {{#group.treeitem}}
-                      Mastiff
-                    {{/group.treeitem}}
-                  {{/treeitem.group}}
-                {{/group.treeitem}}
-              {{/treeitem.group}}
-            {{/group.treeitem}}
-          {{/treeitem.group}}
-        {{/tree.treeitem}}
-        {{#tree.treeitem activeClass="is-active" as |treeitem|}}
-          Minerals
-          {{#treeitem.group as |group|}}
-            {{#group.treeitem}}
-              Zinc
-            {{/group.treeitem}}
-            {{#group.treeitem as |treeitem|}}
-              Gold
-              {{#treeitem.group as |group|}}
-                {{#group.treeitem}}
-                  Yellow Gold
-                {{/group.treeitem}}
-                {{#group.treeitem}}
-                  White Gold
-                {{/group.treeitem}}
-              {{/treeitem.group}}
-            {{/group.treeitem}}
-            {{#group.treeitem}}
-              Silver
-            {{/group.treeitem}}
-          {{/treeitem.group}}
-        {{/tree.treeitem}}
-        {{#tree.treeitem as |treeitem|}}
-          Vegetables
-          {{#treeitem.group as |group|}}
-            {{#group.treeitem}}
-              Carrot
-            {{/group.treeitem}}
-            {{#group.treeitem}}
-              Tomato
-            {{/group.treeitem}}
-            {{#group.treeitem}}
-              Lettuce
-            {{/group.treeitem}}
-          {{/treeitem.group}}
-        {{/tree.treeitem}}
-      {{/ivy-tree}}
-    `);
-  }
+moduleForComponent('ivy-tree', 'Integration | Component | ivy-tree', {
+  integration: true
 });
 
 test('it applies an "aria-hidden" attribute to the children of a collapsed treeitem', function(assert) {
+  this.render(testTemplate);
+
   assert.equal(this.$('[role="treeitem"]:eq(0)').attr('aria-expanded'), 'false');
   assert.equal(this.$('[role="treeitem"]:eq(0) [role="treeitem"]:eq(0)').attr('aria-hidden'), 'true');
 
@@ -125,6 +125,8 @@ test('it applies an "aria-hidden" attribute to the children of a collapsed treei
 });
 
 test('it applies an "aria-level" attribute to treeitems', function(assert) {
+  this.render(testTemplate);
+
   assert.equal(this.$('[role="treeitem"][aria-level="1"]').length, 3);
   assert.equal(this.$('[role="treeitem"][aria-level="2"]').length, 9);
   assert.equal(this.$('[role="treeitem"][aria-level="3"]').length, 7);
@@ -133,6 +135,8 @@ test('it applies an "aria-level" attribute to treeitems', function(assert) {
 });
 
 test('it applies an "aria-selected" attribute to the active treeitem', function(assert) {
+  this.render(testTemplate);
+
   assert.equal(this.$('[role="treeitem"][aria-level="1"]:eq(0)').attr('aria-selected'), 'false');
 
   this.$('[role="treeitem"][aria-level="1"]:eq(0)').click();
@@ -140,6 +144,8 @@ test('it applies an "aria-selected" attribute to the active treeitem', function(
 });
 
 test('it activates a treeitem on click', function(assert) {
+  this.render(testTemplate);
+
   assert.equal(this.$('[role="tree"]').attr('aria-activedescendant'), undefined);
 
   this.$('[role="treeitem"][aria-level="1"]:eq(0)').click();
@@ -147,18 +153,28 @@ test('it activates a treeitem on click', function(assert) {
 });
 
 test('it applies an "active" class to the active treeitem', function(assert) {
+  this.render(testTemplate);
+
   this.$('[role="treeitem"][aria-level="1"]:eq(0)').click();
 
   assert.ok(this.$('[role="treeitem"][aria-level="1"]:eq(0)').hasClass('active'));
 });
 
 test('it applies a custom activeClass class to the active treeitem', function(assert) {
-  this.$('[role="treeitem"][aria-level="1"]:eq(1)').click();
+  this.render(hbs`
+    {{#ivy-tree as |tree|}}
+      {{tree.treeitem activeClass="is-active"}}
+    {{/ivy-tree}}
+  `);
 
-  assert.ok(this.$('[role="treeitem"][aria-level="1"]:eq(1)').hasClass('is-active'));
+  this.$('[role="treeitem"][aria-level="1"]:eq(0)').click();
+
+  assert.ok(this.$('[role="treeitem"][aria-level="1"]:eq(0)').hasClass('is-active'));
 });
 
 test('it toggles expansion of a treeitem on double-click', function(assert) {
+  this.render(testTemplate);
+
   assert.equal(this.$('[role="treeitem"][aria-level="1"]:eq(0)').attr('aria-expanded'), 'false');
 
   this.$('[role="treeitem"][aria-level="1"]:eq(0)').dblclick();
@@ -166,6 +182,8 @@ test('it toggles expansion of a treeitem on double-click', function(assert) {
 });
 
 test('it toggles expansion of the focused treeitem when enter key is pressed', function(assert) {
+  this.render(testTemplate);
+
   this.$('[role="treeitem"][aria-level="1"]:eq(0)').click();
 
   assert.equal(this.$('[role="treeitem"][aria-level="1"]:eq(0)').attr('aria-expanded'), 'false');
@@ -175,6 +193,8 @@ test('it toggles expansion of the focused treeitem when enter key is pressed', f
 });
 
 test('it selects the previous sibling treeitem when up arrow key is pressed', function(assert) {
+  this.render(testTemplate);
+
   this.$('[role="treeitem"][aria-level="1"]:eq(1)').click();
 
   this.$('[role="tree"]').trigger(Ember.$.Event('keydown', { keyCode: 38 }));
@@ -182,6 +202,8 @@ test('it selects the previous sibling treeitem when up arrow key is pressed', fu
 });
 
 test('it selects the parent treeitem when up arrow key is pressed', function(assert) {
+  this.render(testTemplate);
+
   this.$('[role="treeitem"][aria-level="1"]:eq(1)').dblclick();
   this.$('[role="treeitem"][aria-level="1"] [role="treeitem"][aria-level="2"]:eq(0)').click();
 
@@ -190,6 +212,8 @@ test('it selects the parent treeitem when up arrow key is pressed', function(ass
 });
 
 test('it selects the next sibling treeitem when down arrow key is pressed', function(assert) {
+  this.render(testTemplate);
+
   this.$('[role="treeitem"][aria-level="1"]:eq(0)').click();
 
   this.$('[role="tree"]').trigger(Ember.$.Event('keydown', { keyCode: 40 }));
@@ -197,6 +221,8 @@ test('it selects the next sibling treeitem when down arrow key is pressed', func
 });
 
 test('it selects the next sibling of the parent treeitem when down arrow key is pressed', function(assert) {
+  this.render(testTemplate);
+
   this.$('[role="treeitem"][aria-level="1"]:eq(0)').dblclick();
   this.$('[role="treeitem"][aria-level="1"]:eq(0) [role="treeitem"][aria-level="2"]:last-child').click();
 
@@ -205,6 +231,8 @@ test('it selects the next sibling of the parent treeitem when down arrow key is 
 });
 
 test('it collapses the (expanded) currently-focused node when left arrow key is pressed', function(assert) {
+  this.render(testTemplate);
+
   this.$('[role="treeitem"][aria-level="1"]:eq(0)').click();
   this.$('[role="treeitem"][aria-level="1"]:eq(0)').dblclick();
 
@@ -217,6 +245,8 @@ test('it collapses the (expanded) currently-focused node when left arrow key is 
 });
 
 test('it selects the parent of the (collapsed) currently-focused node when left arrow key is pressed', function(assert) {
+  this.render(testTemplate);
+
   this.$('[role="treeitem"][aria-level="1"]:eq(0)').dblclick();
   this.$('[role="treeitem"][aria-level="1"] [role="treeitem"][aria-level="2"]:eq(0)').click();
 
@@ -227,6 +257,8 @@ test('it selects the parent of the (collapsed) currently-focused node when left 
 });
 
 test('it expands a collapsed treeitem and selects its first child when right arrow key is pressed', function(assert) {
+  this.render(testTemplate);
+
   this.$('[role="treeitem"][aria-level="1"]:eq(0)').click();
 
   assert.equal(this.$('[role="treeitem"][aria-level="1"]:eq(0)').attr('aria-expanded'), 'false');
@@ -237,6 +269,8 @@ test('it expands a collapsed treeitem and selects its first child when right arr
 });
 
 test('it expands all treeitems when asterisk (shift+8) key is pressed', function(assert) {
+  this.render(testTemplate);
+
   assert.notEqual(this.$('[role="treeitem"][aria-expanded="false"]').length, 0);
 
   this.$('[role="tree"]').trigger(Ember.$.Event('keydown', { keyCode: 56, shiftKey: true }));
@@ -244,6 +278,8 @@ test('it expands all treeitems when asterisk (shift+8) key is pressed', function
 });
 
 test('it selects the first, visible treeitem when home key is pressed', function(assert) {
+  this.render(testTemplate);
+
   this.$('[role="treeitem"][aria-level="1"]:eq(1)').click();
 
   assert.equal(this.$('[role="treeitem"][aria-level="1"]:eq(0)').attr('aria-selected'), 'false');
@@ -254,6 +290,8 @@ test('it selects the first, visible treeitem when home key is pressed', function
 });
 
 test('it selects the last, visible treeitem when end key is pressed', function(assert) {
+  this.render(testTemplate);
+
   this.$('[role="treeitem"][aria-level="1"]:eq(1)').click();
 
   assert.equal(this.$('[role="treeitem"][aria-level="1"]:eq(2)').attr('aria-selected'), 'false');
