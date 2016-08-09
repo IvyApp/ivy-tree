69794f4899c000a25ad325729d72418cbd1e3c6f
Dray Lacy <dray@envylabs.com>
2016-08-09 14:36:21 -0400

    Add `role` attribute to `ivy-treegroup`

--- addon/components/ivy-treegroup.js
+++ addon/components/ivy-treegroup.js
@@ -2,6 +2,8 @@ import Component from 'ember-component';
 import layout from '../templates/components/ivy-treegroup';
 
 export default Component.extend({
+  ariaRole: 'group',
+
   layout,
 
   tagName: 'ul'
