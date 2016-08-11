3214c99679bb3271c35f2c3838c94ccb301c4d92
Dray Lacy <dray@envylabs.com>
2016-08-11 16:58:53 -0400

    Coerce `isVisible` to a boolean
    This fixes an issue where having `isVisible` set to undefined would
    cause the group to be expanded.

--- addon/components/ivy-treegroup.js
+++ addon/components/ivy-treegroup.js
@@ -1,8 +1,11 @@
 import Component from 'ember-component';
 import layout from '../templates/components/ivy-treegroup';
+import { bool } from 'ember-computed';
 
 export default Component.extend({
   ariaRole: 'group',
+
+  isVisible: bool('isExpanded'),
 
   layout,
 
